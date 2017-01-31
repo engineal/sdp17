30f71352b600156a9b71135ec88e9819e6619b0c
njdu <ndunn@umass.edu>
2017-01-31 10:23:18 -0500

    Filtering is working!

---
+++ Earbeamer/Earbeamer/FIRFilter.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef FIR_FILTER_H_
+#define FIR_FILTER_H_
+
+#include "beamforming.h"
+
+
+
+class FIRFilter {
+
+private:
+	int filsize;
+	double* h;			//filter impulse response
+	double* workspace;	//holds a sample, and a filter's length worth of the previous sample
+
+public:
+	FIRFilter(int ord, double* coef);
+	~FIRFilter();
+	void filter(double (&sample)[BUFFER_LENGTH], double (&buffer)[BUFFER_LENGTH]);
+	void flush();
+
+};
+
+#endif
